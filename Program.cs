2e6eb5e78dbfcc5c69e2dab59acf18f3d33b128c
Ivan Ivanov <137335549+adainian@users.noreply.github.com>
2024-01-22 22:22:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sum_N_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for ( int i = 0; i < n; i++ )
+            {
+                sum += double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
